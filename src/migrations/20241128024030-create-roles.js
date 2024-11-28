2e671c875d3736c3a9cf679aba2bb8c53e03e5dc
CarlosPure <carlos.pure@acklenavenue.com>
2024-11-27 22:23:44 -0500

    tables creation

--- src/migrations/20241128024030-create-roles.js
+++ src/migrations/20241128024030-create-roles.js
@@ -2,21 +2,23 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('roles', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      role_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('roles');
+  },
 };
--- src/migrations/20241128024036-create-doctor-locations.js
+++ src/migrations/20241128024036-create-doctor-locations.js
@@ -2,21 +2,21 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('doctor_locations', {
+      doctor_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'doctors', key: 'id' },
+      },
+      location_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'locations', key: 'id' },
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('doctor_locations');
+  },
 };
